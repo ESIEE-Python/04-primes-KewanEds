e83a138de081dd4a2e1d199e88f74821e80d3890
KewanEds <kewan.edragas@edu.esiee.fr>
2024-12-05 10:11:40 +0000

    Kewan EDRAGAS

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
+""" Calcule et vérifie si un nombre est premier ou pas
+"""
 from math import sqrt
 
 #### Fonction secondaire
-
-
 def isprime(p):
-
-    # votre code ici
-
-    pass
+    """ Indique si le nombre entier entré est premier.
+    Args : 
+        p : Valeur entière positive
+    Returns :
+        isprime(p) : True
+    """
+    premier = True
+    if p in (1,0) :
+        premier = False
+    for k in range(2,int(sqrt(p))+1):
+        if p%k == 0:
+            premier = False
+            break
+    return premier
 
 #### Fonction principale
-
-
 def main():
-
-    # vos appels à la fonction secondaire ici
-
+    """ Fait appel à la fonction secondaire et vérifie son bon fonctionnement
+    Args :
+        none
+    Returns : 
+        none
+    """
+    print(isprime(0), isprime(1), isprime(2), isprime(43))
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
-
     print()
-
 
 if __name__ == "__main__":
     main()
